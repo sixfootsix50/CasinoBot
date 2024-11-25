a5b5c5d697557f0a21780f1d66c95d59661b813f
sixfootsix50 <sixfootsix50@gmail.com>
2024-11-25 11:16:05 -0500

    Added to runPoker function

--- CasinoBot.c
+++ CasinoBot.c
@@ -18,27 +18,34 @@ typedef struct Player{
 } Player;
 Player *playerList;
 
-Card drawCard();
-void resetDeck();
-// Card *testDeck;
+FILE *dataFile;
+FILE *botKey;
+
 Card baseDeck[52] = {{1,'h'},{2,'h'},{3,'h'},{4,'h'},{5,'h'},{6,'h'},{7,'h'},{8,'h'},{9,'h'},{10,'h'},{11,'h'},{12,'h'},{13,'h'},
     {1,'d'},{2,'d'},{3,'d'},{4,'d'},{5,'d'},{6,'d'},{7,'d'},{8,'d'},{9,'d'},{10,'d'},{11,'d'},{12,'d'},{13,'d'},
     {1,'c'},{2,'c'},{3,'c'},{4,'c'},{5,'c'},{6,'c'},{7,'c'},{8,'c'},{9,'c'},{10,'c'},{11,'c'},{12,'c'},{13,'c'},
     {1,'s'},{2,'s'},{3,'s'},{4,'s'},{5,'s'},{6,'s'},{7,'s'},{8,'s'},{9,'s'},{10,'s'},{11,'s'},{12,'s'},{13,'s'}};
 Card Deck[52];
+Card pokerRiver[5];
 int cardsLeft=52;
+int pokerPot;
 
 int getRand();
-void runGame();
+Card drawCard();
+void resetDeck();
+void runPoker();
 void addPlayer(int, int);
 int playerCount;
 
 int main(){
+    dataFile = fopen("userData.bin","wb+");
+    botKey = fopen("botKey.bin","rb");
     resetDeck();
-    runGame();
+    runPoker();
 }
 
-void runGame(){
+void runPoker(){
+    pokerPot = 0;
     printf("How many players?");
     scanf("%d",&playerCount);
     addPlayer(1,50);
@@ -54,6 +61,24 @@ void runGame(){
         printf("%d, %c\n", currentPlayer->card2.number, currentPlayer->card2.suite);
         currentPlayer = currentPlayer->next;
     }
+    takeBets();
+    pokerRiver[0] = drawCard();
+    pokerRiver[1] = drawCard();
+    pokerRiver[2] = drawCard();
+    printf("%d, %c, ", pokerRiver[0].number, pokerRiver[0].suite);
+    printf("%d, %c, ", pokerRiver[1].number, pokerRiver[1].suite);
+    printf("%d, %c\n", pokerRiver[2].number, pokerRiver[2].suite);
+
+    takeBets();
+    pokerRiver[3] = drawCard();
+    printf("%d, %c\n", pokerRiver[3].number, pokerRiver[3].suite);
+
+    takeBets();
+    pokerRiver[4] = drawCard();
+    printf("%d, %c\n", pokerRiver[4].number, pokerRiver[4].suite);
+
+    takeBets();
+    checkPokerWinner();
 }
 
 void takeBets(){ //TODO: add ability for players to make bets
@@ -62,6 +87,7 @@ void takeBets(){ //TODO: add ability for players to make bets
     Player *currentPlayer = playerList;
     while (finished != 1){
         currentPlayer->bet += newBet;
+        pokerPot += newBet;
     }
 }
 
@@ -93,6 +119,6 @@ void resetDeck(){
     memcpy(Deck, baseDeck, sizeof(Deck));
 }
 
-void checkWinner(){
+void checkPokerWinner(){
     //TODO: check for player with winning hand
 }
